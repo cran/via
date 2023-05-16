6cbde4c3f341c53ac401270056a6abe90b33cef8
Adam T. Kocsis <adam.t.kocsis@gmail.com>
2023-05-16 12:50:02 +0000

    version 0.2.0

--- R/SfcArray-sf.R
+++ R/SfcArray-sf.R
@@ -1,7 +1,7 @@
 #' @rdname st_crs
-#' @method st_crs SfArray
+#' @method st_crs SfcArray
 #' @export 
-st_crs.SfArray <- function(x,...){
+st_crs.SfcArray <- function(x,...){
 	# CRS should be the same in the entire stack
 	if(!requireNamespace("sf", quietly=TRUE)) stop("This function requires the 'sf' package to run.")
 	crs <- sf::st_crs(x@stack[[1]],...)
@@ -10,9 +10,9 @@ st_crs.SfArray <- function(x,...){
 }
 
 #' @rdname st_transform
-#' @method st_transform SfArray
+#' @method st_transform SfcArray
 #' @export 
-st_transform.SfArray <- function(x,...){
+st_transform.SfcArray <- function(x,...){
 	# CRS should be the same in the entire stack
 	if(!requireNamespace("sf", quietly=TRUE)) stop("This function requires the 'sf' package to run.")
 	for(i in 1:length(x@stack)){
@@ -22,16 +22,16 @@ st_transform.SfArray <- function(x,...){
 }
 
 #' @rdname st_bbox
-#' @method st_bbox SfArray
+#' @method st_bbox SfcArray
 #' @export 
-st_bbox.SfArray <- function(x,...){
+st_bbox.SfcArray <- function(obj,...){
 	# CRS should be the same in the entire stack
 	if(!requireNamespace("sf", quietly=TRUE)) stop("This function requires the 'sf' package to run.")
 	m <- NULL
-	for(i in 1:length(x@stack)){
-		m<- rbind(m, sf::st_bbox(x@stack[[i]]))
+	for(i in 1:length(obj@stack)){
+		m<- rbind(m, sf::st_bbox(obj@stack[[i]]))
 	}
-	final <- sf::st_bbox(x@stack[[1]])
+	final <- sf::st_bbox(obj@stack[[1]])
 	final["xmin"] <- min(m[,"xmin"])
 	final["ymin"] <- min(m[,"ymin"])
 	final["xmax"] <- max(m[,"xmax"])
